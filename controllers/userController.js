90d56f11fd278f6bb14317f9937f7b6b53d26217
James Harding <99635678+JaHa675@users.noreply.github.com>
2022-05-25 15:31:47 -0700

    bug fixin

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,7 +8,7 @@ module.exports = {
     },
     getSingleUser(req, res) {
         User.findOne({ _id: req.params.userId })
-        .select('-__v')
+            .select('-__v')
             .then((user) =>
                 !user
                     ? res.status(404).json({ message: "No user with that ID" })
@@ -86,10 +86,11 @@ module.exports = {
             )
             .catch((err) => res.status(500).json(err));
     },
+
     removeFriend(req, res) {
         User.findOneAndUpdate(
             { _id: req.params.userId },
-            { $pull: { friends: { friendId: req.params.friendId } } },
+            { $pull: { friends: { _id: req.params.friendId } } },
             { runValidators: true, new: true }
         )
             .then((user) =>
